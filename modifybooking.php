36185ea680eb2f02e2c063f807bbc11582e32e6e
AdnanHabibAbir <67456659+AdnanHabibAbir@users.noreply.github.com>
2020-09-11 03:23:07 +0600

    Add files via upload

---
+++ modifybooking.php
@@ -0,0 +1,25 @@
+<html>
+<head> 
+<link rel="stylesheet" type="text/css" href= "https://bootswatch.com/4/flatly/bootstrap.css">
+
+
+
+
+</head>
+<body style="background: url(https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80); background-size:100% 100%;">
+<center>
+<center>
+<font size = "10">Modify Page</font><br><br><br><br>
+<button type ="button"><a href = "addbook.php"><font size="4">Add booking</font></a></button></form><br><br>
+<button type ="button"><a href = "remove.php"><font size="4">Remove booking</font></a></button><br><br>
+<button type ="button"><a href = "modify.php"><font size="4">Modify booking</a></font></button><br><br>
+
+
+<script src = "https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"> </script>
+
+<script src = "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"> </script>
+</body>
+</center>
+</html>
